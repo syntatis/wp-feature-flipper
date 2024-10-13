a6e9f0c6a26da0e9d04e7224d12b6f31e274e5c1
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-10-13 21:44:26 +0700

    Remove class comments

--- app/Plugin.php
+++ app/Plugin.php
@@ -8,20 +8,9 @@ use SSFV\Codex\Contracts\Extendable;
 use SSFV\Codex\Settings\Settings;
 use SSFV\Psr\Container\ContainerInterface;
 
-/**
- * The Plugin class.
- *
- * Serves as the main entry point for plugin, handling the initialization
- * of core functionalities manages activation, deactivation, and update
- * processes.
- */
 class Plugin implements Extendable
 {
-	/**
-	 * Provide the plugin's feature to instantiate.
-	 *
-	 * @return iterable<object>
-	 */
+	/** @return iterable<object> */
 	public function getInstances(ContainerInterface $container): iterable
 	{
 		yield new SettingPage($container->get(Settings::class));
