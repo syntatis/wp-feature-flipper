a04c02eaacae390197bb33d101c5b9b6fbd63e26
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-10-13 23:44:22 +0700

    Add custom init hook for the plugin

--- app/Plugin.php
+++ app/Plugin.php
@@ -13,12 +13,16 @@ class Plugin implements Extendable
 	/** @return iterable<object> */
 	public function getInstances(ContainerInterface $container): iterable
 	{
+		// Add the setting page.
 		yield new SettingPage($container->get(Settings::class));
 
-		// Switches.
+		// Apply the feature switches.
 		yield new Switches\Admin();
 		yield new Switches\Assets();
 		yield new Switches\General();
 		yield new Switches\Media();
+
+		// Mark as initilized.
+		do_action('syntatis/feature-flipper/init', $container);
 	}
 }
