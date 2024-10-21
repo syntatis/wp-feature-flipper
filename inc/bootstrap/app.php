c78edae97cce7e65b35eed7ec38c3d264a88ed88
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-10-21 19:09:38 +0700

    Remove manual require

--- inc/bootstrap/app.php
+++ inc/bootstrap/app.php
@@ -7,7 +7,6 @@ namespace Syntatis\FeatureFlipper;
 use SSFV\Codex;
 
 use function defined;
-use function file_exists;
 
 // If this file is called directly, abort.
 if (! defined('ABSPATH')) {
@@ -30,15 +29,6 @@ if (! defined('ABSPATH')) {
  * @see https://github.com/humbug/php-scoper
  */
 require PLUGIN_DIR . '/dist/autoload/vendor/scoper-autoload.php';
-
-/**
- * Load the environment-specific application runtime.
- */
-$appEnv = PLUGIN_DIR . '/inc/bootstrap/app.' . wp_get_environment_type() . '.php';
-
-if (file_exists($appEnv)) {
-	require $appEnv;
-}
 
 /**
  * Initialize the plugin application.
