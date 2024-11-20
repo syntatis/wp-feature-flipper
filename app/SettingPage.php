e8e2a32bcbf7028fb1b685f0a1014f22fd6c7aa0
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-11-20 10:08:39 +0700

    Add "Settings" link in plugin table list (#25)

--- app/SettingPage.php
+++ app/SettingPage.php
@@ -12,6 +12,8 @@ use WP_REST_Request;
 
 use function array_filter;
 use function array_keys;
+use function array_merge;
+use function basename;
 use function function_exists;
 use function in_array;
 use function is_array;
@@ -25,17 +27,18 @@ class SettingPage implements Hookable
 {
 	private Settings $settings;
 
-	private string $handle;
+	private string $appName;
+
+	private string $scriptHandle;
 
 	private string $pageName;
 
 	public function __construct(Settings $settings)
 	{
-		$name = App::name();
-
+		$this->appName = App::name();
 		$this->settings = $settings;
-		$this->handle = $name . '-settings';
-		$this->pageName = 'settings_page_' . $name;
+		$this->scriptHandle = $this->appName . '-settings';
+		$this->pageName = 'settings_page_' . $this->appName;
 	}
 
 	public function hook(Hook $hook): void
@@ -43,6 +46,7 @@ class SettingPage implements Hookable
 		$hook->addAction('admin_menu', [$this, 'addMenu']);
 		$hook->addAction('admin_enqueue_scripts', [$this, 'enqueueAdminScripts']);
 		$hook->addAction('admin_bar_menu', [$this, 'addAdminInlineScripts'], PHP_INT_MAX);
+		$hook->addFilter(sprintf('plugin_action_links'), [$this, 'addPluginActionLinks'], 10, 2);
 	}
 
 	/**
@@ -97,21 +101,21 @@ class SettingPage implements Hookable
 		$assets = is_readable($assets) ? require $assets : [];
 
 		wp_enqueue_style(
-			$this->handle,
+			$this->scriptHandle,
 			App::url('dist/assets/setting-page/index.css'),
 			[],
 			$assets['version'] ?? null,
 		);
 
 		wp_enqueue_script(
-			$this->handle,
+			$this->scriptHandle,
 			App::url('dist/assets/setting-page/index.js'),
 			$assets['dependencies'] ?? [],
 			$assets['version'] ?? null,
 			true,
 		);
 
-		wp_set_script_translations($this->handle, 'syntatis-feature-flipper');
+		wp_set_script_translations($this->scriptHandle, 'syntatis-feature-flipper');
 	}
 
 	public function addAdminInlineScripts(): void
@@ -121,10 +125,32 @@ class SettingPage implements Hookable
 		}
 
 		wp_add_inline_script(
-			$this->handle,
+			$this->scriptHandle,
 			$this->getInlineScript(),
 			'before',
 		);
+	}
+
+	/**
+	 * @param array<string> $links
+	 *
+	 * @return array<string>
+	 */
+	public function addPluginActionLinks(array $links, string $pluginFile): array
+	{
+		if (basename($pluginFile, '.php') !== $this->appName) {
+			return $links;
+		}
+
+		$links = array_merge([
+			sprintf(
+				'<a href="%1$s">%2$s</a>',
+				get_admin_url(null, 'options-general.php?page=' . $this->appName),
+				__('Settings', 'syntatis-feature-flipper'),
+			),
+		], $links);
+
+		return $links;
 	}
 
 	/**
