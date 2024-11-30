5eb89daaa0125a5638ab020e1e72e740ffc0b7eb
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-11-30 12:41:37 +0700

    Add class to manage core updates

--- app/Features/Updates/ManageCore.php
+++ app/Features/Updates/ManageCore.php
@@ -8,24 +8,39 @@ use SSFV\Codex\Contracts\Hookable;
 use SSFV\Codex\Foundation\Hooks\Hook;
 use Syntatis\FeatureFlipper\Option;
 
+use function define;
+use function defined;
 use function property_exists;
 
 class ManageCore implements Hookable
 {
 	public function hook(Hook $hook): void
 	{
-		if ((bool) Option::get('updates_core')) {
+		if (! (bool) Option::get('updates_core')) {
+			$hook->addAction('schedule_event', [$this, 'filterCronEvents']);
+			$hook->addFilter('send_core_update_notification_email', static fn (): bool => false);
+			$hook->addAction('admin_init', static function () use ($hook): void {
+				$hook->removeAction('admin_init', '_maybe_update_core');
+				$hook->removeAction('admin_init', 'wp_auto_update_core');
+				$hook->removeAction('admin_init', 'wp_maybe_auto_update');
+				$hook->removeAction('wp_maybe_auto_update', 'wp_maybe_auto_update');
+				$hook->removeAction('wp_version_check', 'wp_version_check');
+			});
+		}
+
+		if ((bool) Option::get('auto_update_core')) {
 			return;
 		}
 
-		$hook->addAction('schedule_event', [$this, 'filterCronEvents']);
-		$hook->addAction('admin_init', static function () use ($hook): void {
-			$hook->removeAction('admin_init', '_maybe_update_core');
-			$hook->removeAction('admin_init', 'wp_auto_update_core');
-			$hook->removeAction('admin_init', 'wp_maybe_auto_update');
-			$hook->removeAction('wp_maybe_auto_update', 'wp_maybe_auto_update');
-			$hook->removeAction('wp_version_check', 'wp_version_check');
-		});
+		if (! defined('WP_AUTO_UPDATE_CORE')) {
+			define('WP_AUTO_UPDATE_CORE', false);
+		}
+
+		$hook->addFilter('allow_dev_auto_core_updates', static fn (): bool => false);
+		$hook->addFilter('allow_major_auto_core_updates', static fn (): bool => false);
+		$hook->addFilter('allow_minor_auto_core_updates', static fn (): bool => false);
+		$hook->addFilter('auto_core_update_send_email', static fn (): bool => false);
+		$hook->addFilter('auto_update_core', static fn (): bool => false);
 	}
 
 	/** @return object|false */
