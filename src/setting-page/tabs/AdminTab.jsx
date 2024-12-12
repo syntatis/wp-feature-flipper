b0df35df78ef351df85aba63d30eff7c1f4fbe28
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-12-12 18:53:00 +0700

    Remove option when the related option is disabled (#36)

--- src/setting-page/tabs/AdminTab.jsx
+++ src/setting-page/tabs/AdminTab.jsx
@@ -1,5 +1,5 @@
 import { __ } from '@wordpress/i18n';
-import { Fieldset, Form } from '../form';
+import { Fieldset, Form, useSettingsContext } from '../form';
 import { AdminBarInputs, DashboardWidgetsInputs, SwitchInput } from '../inputs';
 import { HelpContent } from '../components';
 
@@ -7,6 +7,7 @@ const wpFooter = document.querySelector( '#wpfooter' );
 const wpFooterDisplayStyle = wpFooter?.style?.display;
 
 export const AdminTab = () => {
+	const { getOption } = useSettingsContext();
 	return (
 		<Form>
 			<Fieldset>
@@ -31,29 +32,34 @@ export const AdminTab = () => {
 						}
 					} }
 				/>
-				<SwitchInput
-					name="update_nags"
-					id="update-nags"
-					help={
-						<HelpContent>
-							<p>
-								{ __(
-									'This option will only remove the update notice that appear at the top of the admin area.It does not prevent the updates itself. To disable the updates, you can switch them off from the "General › Advanced › Updates" option.',
-									'syntatis-feature-flipper'
-								) }
-							</p>
-						</HelpContent>
-					}
-					title={ __( 'Update Nags', 'syntatis-feature-flipper' ) }
-					label={ __(
-						'Enable WordPress update notification message',
-						'syntatis-feature-flipper'
-					) }
-					description={ __(
-						'When switched off, WordPress will not show notification message when update is available.',
-						'syntatis-feature-flipper'
-					) }
-				/>
+				{ getOption( 'updates' ) && (
+					<SwitchInput
+						name="update_nags"
+						id="update-nags"
+						help={
+							<HelpContent>
+								<p>
+									{ __(
+										'This option will only remove the update notice that appear at the top of the admin area.It does not prevent the updates itself. To disable the updates, you can switch them off from the "General › Advanced › Updates" option.',
+										'syntatis-feature-flipper'
+									) }
+								</p>
+							</HelpContent>
+						}
+						title={ __(
+							'Update Nags',
+							'syntatis-feature-flipper'
+						) }
+						label={ __(
+							'Enable WordPress update notification message',
+							'syntatis-feature-flipper'
+						) }
+						description={ __(
+							'When switched off, WordPress will not show notification message when update is available.',
+							'syntatis-feature-flipper'
+						) }
+					/>
+				) }
 			</Fieldset>
 			<Fieldset
 				title={ __( 'Admin Bar', 'syntatis-feature-flipper' ) }
--- src/setting-page/tabs/SiteTab.jsx
+++ src/setting-page/tabs/SiteTab.jsx
@@ -1,9 +1,11 @@
 import { __ } from '@wordpress/i18n';
-import { Fieldset, Form } from '../form';
+import { Fieldset, Form, useSettingsContext } from '../form';
 import { SwitchInput } from '../inputs';
 import { HelpContent } from '../components';
 
 export const SiteTab = () => {
+	const { getOption } = useSettingsContext();
+
 	return (
 		<Form>
 			<Fieldset
@@ -13,35 +15,37 @@ export const SiteTab = () => {
 					'syntatis-feature-flipper'
 				) }
 			>
-				<SwitchInput
-					name="rsd_link"
-					id="rsd-link"
-					title={ __( 'RSD Link', 'syntatis-feature-flipper' ) }
-					label={ __(
-						'Enable RSD link',
-						'syntatis-feature-flipper'
-					) }
-					description={ __(
-						'When switched off, it will remove the Really Simple Discovery (RSD) link from the webpage head.',
-						'syntatis-feature-flipper'
-					) }
-					help={
-						<HelpContent>
-							<p>
-								{ __(
-									'The RSD link, or "Really Simple Discovery", helps external applications detect services like publishing APIs on your site. However, it\'s mostly outdated and rarely used today, so removing it from your site is usually safe.',
-									'syntatis-feature-flipper'
-								) }
-							</p>
-							<p>
-								{ __(
-									'Note that this action only removes the link itself, not the underlying RSD endpoint. To fully disable the endpoint, you can turn off XML-RPC from "Security › XML-RPC".',
-									'syntatis-feature-flipper'
-								) }
-							</p>
-						</HelpContent>
-					}
-				/>
+				{ getOption( 'xmlrpc' ) && (
+					<SwitchInput
+						name="rsd_link"
+						id="rsd-link"
+						title={ __( 'RSD Link', 'syntatis-feature-flipper' ) }
+						label={ __(
+							'Enable RSD link',
+							'syntatis-feature-flipper'
+						) }
+						description={ __(
+							'When switched off, it will remove the Really Simple Discovery (RSD) link from the webpage head.',
+							'syntatis-feature-flipper'
+						) }
+						help={
+							<HelpContent>
+								<p>
+									{ __(
+										'The RSD link, or "Really Simple Discovery", helps external applications detect services like publishing APIs on your site. However, it\'s mostly outdated and rarely used today, so removing it from your site is usually safe.',
+										'syntatis-feature-flipper'
+									) }
+								</p>
+								<p>
+									{ __(
+										'Note that this action only removes the link itself, not the underlying RSD endpoint. To fully disable the endpoint, you can turn off XML-RPC from "Security › XML-RPC".',
+										'syntatis-feature-flipper'
+									) }
+								</p>
+							</HelpContent>
+						}
+					/>
+				) }
 				<SwitchInput
 					name="generator_tag"
 					id="generator-tag"
