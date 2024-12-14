cc8461eefbbbe1adcfeaaa34c48d54ef1e96cd6c
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-12-14 22:34:25 +0700

    Enqueu common shared scripts

--- app/CommonScripts.php
+++ app/CommonScripts.php
@@ -5,17 +5,29 @@ declare(strict_types=1);
 namespace Syntatis\FeatureFlipper;
 
 use SSFV\Codex\Contracts\Hookable;
+use SSFV\Codex\Facades\App;
 use SSFV\Codex\Foundation\Hooks\Hook;
 
 use function sprintf;
 
 use const PHP_INT_MAX;
 
-class AdminPage implements Hookable
+class CommonScripts implements Hookable
 {
 	public function hook(Hook $hook): void
 	{
 		$hook->addAction('admin_bar_menu', [$this, 'addInlineScript'], PHP_INT_MAX);
+		$hook->addAction('wp_enqueue_scripts', [$this, 'enqueueScripts'], PHP_INT_MAX);
+		$hook->addAction('admin_enqueue_scripts', [$this, 'enqueueScripts'], PHP_INT_MAX);
+	}
+
+	public function enqueueScripts(): void
+	{
+		if (! is_user_logged_in()) {
+			return;
+		}
+
+		wp_enqueue_style(App::name(), App::url('dist/assets/index.css'));
 	}
 
 	public function addInlineScript(): void
@@ -25,16 +37,18 @@ class AdminPage implements Hookable
 
 	private function getInlineScript(): string
 	{
+		$envType = wp_get_environment_type();
+
 		return sprintf(
 			<<<'SCRIPT'
 			window.$syntatis = { featureFlipper: %s };
 			SCRIPT,
 			wp_json_encode(
 				apply_filters('syntatis/feature_flipper/inline_data', [
+					'environmentType' => $envType,
 					'themeSupport' => [
 						'widgetsBlockEditor' => get_theme_support('widgets-block-editor'),
 					],
-					'environmentType' => wp_get_environment_type(),
 				]),
 			),
 		);
