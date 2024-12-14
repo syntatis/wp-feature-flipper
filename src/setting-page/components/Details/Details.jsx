905efeebb9ff9fcaa16e75b42abdddaf5daf7fc6
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-12-14 13:15:20 +0700

    Improve `details` styles

--- src/setting-page/components/Details/Details.jsx
+++ src/setting-page/components/Details/Details.jsx
@@ -1,10 +1,10 @@
 import styles from './Details.module.scss';
 
-export const Details = ( { summary, children } ) => {
+export const Details = ( { summary, children, className = styles.root } ) => {
 	return (
-		<details className={ styles.root }>
+		<details className={ className }>
 			<summary>{ summary }</summary>
-			{ children }
+			<div className={ styles.content }>{ children }</div>
 		</details>
 	);
 };
