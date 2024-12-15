40cfe2aef94f1184b17bb5c67874656b5f717bdf
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-12-15 14:34:39 +0700

    Merge `Assets` to `Site` section

--- app/Modules/Site.php
+++ app/Modules/Site.php
@@ -7,22 +7,62 @@ namespace Syntatis\FeatureFlipper\Modules;
 use SSFV\Codex\Contracts\Hookable;
 use SSFV\Codex\Foundation\Hooks\Hook;
 use Syntatis\FeatureFlipper\Helpers\Option;
+use WP_Scripts;
+
+use function array_diff;
 
 class Site implements Hookable
 {
 	public function hook(Hook $hook): void
 	{
-		// 1. RSD Link.
+		if (! (bool) Option::get('emojis')) {
+			/**
+			 * WordPress 6.4 deprecated the use of `print_emoji_styles` function, but it has
+			 * been retained for backward compatibility purposes.
+			 *
+			 * @see https://make.wordpress.org/core/2023/10/17/replacing-hard-coded-style-tags-with-wp_add_inline_style/
+			 */
+			$hook->removeAction('admin_print_scripts', 'print_emoji_detection_script');
+			$hook->removeAction('admin_print_styles', 'print_emoji_styles');
+			$hook->removeAction('wp_head', 'print_emoji_detection_script', 7);
+			$hook->removeAction('wp_print_styles', 'print_emoji_styles');
+			$hook->removeFilter('comment_text_rss', 'wp_staticize_emoji');
+			$hook->removeFilter('the_content_feed', 'wp_staticize_emoji');
+			$hook->removeFilter('wp_mail', 'wp_staticize_emoji_for_email');
+		}
+
+		if (! (bool) Option::get('scripts_version')) {
+			$callback = static function (string $src): string {
+				return remove_query_arg('ver', $src);
+			};
+			$hook->addFilter('script_loader_src', $callback);
+			$hook->addFilter('style_loader_src', $callback);
+		}
+
+		if (! (bool) Option::get('jquery_migrate')) {
+			$hook->addAction('wp_default_scripts', static function (WP_Scripts $scripts): void {
+				if (! isset($scripts->registered['jquery'])) {
+					return;
+				}
+
+				$script = $scripts->registered['jquery'];
+
+				if ($script->deps === []) {
+					return;
+				}
+
+				$script->deps = array_diff($script->deps, ['jquery-migrate']);
+			});
+		}
+
 		if (! (bool) Option::get('rsd_link')) {
 			$hook->removeAction('wp_head', 'rsd_link');
 		}
 
-		// 2. WordPress Generator meta tag.
 		if (! (bool) Option::get('generator_tag')) {
 			$hook->removeAction('wp_head', 'wp_generator');
 		}
 
-		// 3. Shortlink.
 		if ((bool) Option::get('shortlink')) {
 			return;
 		}
--- src/setting-page/tabs/SiteTab.jsx
+++ src/setting-page/tabs/SiteTab.jsx
@@ -9,6 +9,68 @@ export const SiteTab = () => {
 	return (
 		<Form>
 			<Fieldset
+				title={ __( 'Assets', 'syntatis-feature-flipper' ) }
+				description={ __(
+					'Control the behavior of scripts, styles, and images loaded on the site.',
+					'syntatis-feature-flipper'
+				) }
+			>
+				<SwitchInput
+					name="emojis"
+					id="emojis"
+					title={ __( 'Emojis', 'syntatis-feature-flipper' ) }
+					label={ __(
+						'Enable the WordPress built-in emojis',
+						'syntatis-feature-flipper'
+					) }
+					description={ __(
+						'When switched off, WordPress will not load the emojis scripts, styles, and images.',
+						'syntatis-feature-flipper'
+					) }
+					help={ __(
+						"While Emojis are fun, if you're not using it in your posts or pages, you can safely disable them to reduce the number of requests from the additional scripts and styles require to load them.",
+						'syntatis-feature-flipper'
+					) }
+				/>
+				<SwitchInput
+					name="scripts_version"
+					id="scripts-version"
+					title={ __(
+						'Scripts Version',
+						'syntatis-feature-flipper'
+					) }
+					label={ __(
+						'Show scripts and styles version',
+						'syntatis-feature-flipper'
+					) }
+					description={ __(
+						'When switched off, WordPress will not append the version to the scripts and styles URLs.',
+						'syntatis-feature-flipper'
+					) }
+					help={ __(
+						'The version script specifies the version of a enqueued script in the URL query string like "?ver=5.9.3". This helps browsers detect updates and load the latest script instead of using an outdated cached one. Keeping this parameter enabled is recommended to ensure scripts are update correctly.',
+						'syntatis-feature-flipper'
+					) }
+				/>
+				<SwitchInput
+					name="jquery_migrate"
+					id="jquery-migrate"
+					title={ __( 'jQuery Migrate', 'syntatis-feature-flipper' ) }
+					label={ __(
+						'Load jQuery Migrate script',
+						'syntatis-feature-flipper'
+					) }
+					description={ __(
+						'When switched off, WordPress will not load the jQuery Migrate script.',
+						'syntatis-feature-flipper'
+					) }
+					help={ __(
+						'jQuery Migrate is a WordPress library that ensures older themes and plugins work with newer versions of jQuery. This helps developers update their code while keeping older sites functional. If your site is up-to-date, you can safely disable jQuery Migrate.',
+						'syntatis-feature-flipper'
+					) }
+				/>
+			</Fieldset>
+			<Fieldset
 				title="Metadata"
 				description={ __(
 					'Control the document metadata added in the HTML head.',
