2c04299c620aaeb925e475d3a6744b748a4146c9
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-12-20 18:04:48 +0700

    Rename to `HasHookname`

--- app/Concerns/HasHookName.php
+++ app/Concerns/HasHookName.php
@@ -6,16 +6,16 @@ namespace Syntatis\FeatureFlipper\Concerns;
 
 use Syntatis\FeatureFlipper\Helpers\Option;
 
-trait WPFilterNames
+trait HasHookName
 {
 	/** @phpstan-param non-empty-string $name */
-	private static function optionFilterName(string $name): string
+	private static function optionName(string $name): string
 	{
 		return 'option_' . Option::name($name);
 	}
 
 	/** @phpstan-param non-empty-string $name */
-	private static function defaultOptionFilterName(string $name): string
+	private static function defaultOptionName(string $name): string
 	{
 		return 'default_option_' . Option::name($name);
 	}
