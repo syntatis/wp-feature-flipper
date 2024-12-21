2d258c83b2b6c1841efb6a0b3e94ef6468b9732e
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-12-22 00:00:07 +0700

    Add post types

--- app/CommonScripts.php
+++ app/CommonScripts.php
@@ -7,12 +7,14 @@ namespace Syntatis\FeatureFlipper;
 use SSFV\Codex\Contracts\Hookable;
 use SSFV\Codex\Facades\App;
 use SSFV\Codex\Foundation\Hooks\Hook;
+use WP_Post_Type;
 
 use function array_filter;
-use function array_keys;
+use function array_map;
 use function in_array;
 use function sprintf;
 
+use const ARRAY_FILTER_USE_BOTH;
 use const PHP_INT_MAX;
 
 class CommonScripts implements Hookable
@@ -47,15 +49,29 @@ class CommonScripts implements Hookable
 			wp_json_encode(
 				apply_filters('syntatis/feature_flipper/inline_data', [
 					'environmentType' => wp_get_environment_type(),
-					'postTypes' => array_keys(array_filter(
-						get_post_types(['public' => true]),
-						static fn ($postType) => ! in_array($postType, ['attachment'], true),
-					)),
+					'postTypes' => $this->getPostTypes(),
 					'themeSupport' => [
 						'widgetsBlockEditor' => get_theme_support('widgets-block-editor'),
 					],
 				]),
 			),
+		);
+	}
+
+	/** @return array<string,array<string,mixed>> */
+	private function getPostTypes(): array
+	{
+		$postTypes = array_filter(
+			get_post_types(['public' => true], 'objects'),
+			static fn (WP_Post_Type $postTypeObject, string $postType) => ! in_array($postTypeObject->name, ['attachment'], true),
+			ARRAY_FILTER_USE_BOTH,
+		);
+
+		return array_map(
+			static fn (WP_Post_Type $postTypeObject): array => [
+				'label' => $postTypeObject->label,
+			],
+			$postTypes,
 		);
 	}
 }
