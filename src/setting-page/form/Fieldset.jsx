97c14c65ba0635e875844e7c92f8e8941bf9307e
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-12-21 21:28:42 +0700

    Refactor form and fieldset styles

--- src/setting-page/form/Fieldset.jsx
+++ src/setting-page/form/Fieldset.jsx
@@ -2,18 +2,19 @@ import styles from './Fieldset.module.scss';
 
 export const Fieldset = ( { children, title, description } ) => {
 	return (
-		<div className={ styles.section }>
+		<>
 			{ title ? (
 				<h2 className={ `title ${ styles.title }` }>{ title }</h2>
 			) : null }
 			{ description ? (
 				<p className={ styles.description }>{ description }</p>
 			) : null }
-			<fieldset>
-				<table className="form-table" role="presentation">
-					<tbody>{ children }</tbody>
-				</table>
-			</fieldset>
-		</div>
+			<table
+				className={ `form-table ${ styles.table }` }
+				role="presentation"
+			>
+				<tbody>{ children }</tbody>
+			</table>
+		</>
 	);
 };
