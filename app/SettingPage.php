f58dfe89a915936acbb331a55d5785ed19012915
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-01-02 23:16:16 +0700

    Add `-common` suffix

--- app/SettingPage.php
+++ app/SettingPage.php
@@ -126,7 +126,7 @@ class SettingPage implements Hookable
 		wp_enqueue_style(
 			$this->scriptHandle,
 			App::url('dist/assets/setting-page/index.css'),
-			[$this->appName],
+			[$this->appName . '-common'],
 			$assets['version'] ?? null,
 		);
 
