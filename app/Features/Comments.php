402400f2286a846470c955b8be528901a1f8ac23
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-01-05 14:44:25 +0700

    Update filters to handle excluded post types

--- app/Features/Comments.php
+++ app/Features/Comments.php
@@ -7,11 +7,14 @@ namespace Syntatis\FeatureFlipper\Features;
 use SSFV\Codex\Contracts\Hookable;
 use SSFV\Codex\Facades\App;
 use SSFV\Codex\Foundation\Hooks\Hook;
+use Syntatis\FeatureFlipper\Concerns\WithAdmin;
 use Syntatis\FeatureFlipper\Helpers\Option;
 use WP_Admin_Bar;
 use WP_Block_Type_Registry;
 use WP_Comment;
+use WP_Comment_Query;
 
+use function in_array;
 use function is_readable;
 use function remove_post_type_support;
 
@@ -21,36 +24,72 @@ class Comments implements Hookable
 {
 	use WithAdmin;
 
+	private const EXCLUDE_POST_TYPES = ['product'];
+
 	public function hook(Hook $hook): void
 	{
 		if (Option::isOn('comments')) {
 			return;
 		}
 
+		$hook->addFilter('rest_endpoints', [$this, 'filterRestEndpoints'], PHP_INT_MAX);
+		$hook->addFilter('xmlrpc_methods', [$this, 'filterXmlrpcMethods'], PHP_INT_MAX);
+
+		/**
+		 * Remove comments UI from the admin area.
+		 */
 		$hook->addAction('admin_bar_menu', [$this, 'removeAdminBarMenu'], PHP_INT_MAX);
 		$hook->addAction('admin_init', [$this, 'removePostTypeSupport'], PHP_INT_MAX);
 		$hook->addAction('admin_menu', [$this, 'removeAdminMenu'], PHP_INT_MAX);
-		$hook->addAction('init', [$this, 'removeBlocks'], PHP_INT_MAX);
-		$hook->addAction('enqueue_block_editor_assets', [$this, 'disableOnBlockEditor'], PHP_INT_MAX);
 		$hook->addAction('do_meta_boxes', [$this, 'removePostMetabox'], PHP_INT_MAX);
-		$hook->addFilter('rest_endpoints', [$this, 'restEndpoints'], PHP_INT_MAX);
-		$hook->addFilter('xmlrpc_methods', [$this, 'xmlrpcMethods'], PHP_INT_MAX);
-		$hook->addFilter('comments_array', static fn () => [], PHP_INT_MAX);
-		$hook->addFilter('comments_open', static fn () => false, PHP_INT_MAX);
-		$hook->addFilter('comments_pre_query', static fn () => 0, PHP_INT_MAX);
-		$hook->addFilter('pings_open', static fn () => false, PHP_INT_MAX);
+
+		/**
+		 * Remove Core comments blocks from the block editor.
+		 */
+		$hook->addAction('init', [$this, 'unregisterBlocksServer'], PHP_INT_MAX);
+		$hook->addAction('enqueue_block_editor_assets', [$this, 'unregisterBlocksClient'], PHP_INT_MAX);
+
+		/**
+		 * Modify the comments query to prevent comments from being displayed in the
+		 * "Dashboard".
+		 */
+		$hook->addFilter('comments_pre_query', [$this, 'filterCommentsPreQuery'], PHP_INT_MAX, 2);
+		$hook->addFilter('wp_count_comments', [$this, 'filterCommentsCount'], PHP_INT_MAX, 2);
+
+		/**
+		 * Prevent comments from being displayed on the theme/template.
+		 *
+		 * @see https://github.com/WordPress/WordPress/blob/master/wp-includes/comment-template.php
+		 */
+		$hook->addFilter('comments_array', [$this, 'filterCommentsArray'], PHP_INT_MAX, 2);
+		$hook->addFilter('comments_open', [$this, 'filterCommentsOpen'], PHP_INT_MAX, 2);
+		$hook->addFilter('get_comments_number', [$this, 'filterGetCommentsNumber'], PHP_INT_MAX, 2);
+		$hook->addFilter('pings_open', [$this, 'filterPingsOpen'], PHP_INT_MAX, 2);
 	}
 
 	public function removePostTypeSupport(): void
 	{
 		foreach (get_post_types() as $postType) {
-			if (! post_type_supports($postType, 'comments')) {
+			if (
+				! post_type_supports($postType, 'comments') ||
+				in_array($postType, self::EXCLUDE_POST_TYPES, true)
+			) {
 				continue;
 			}
 
 			remove_post_type_support($postType, 'comments');
 			remove_post_type_support($postType, 'trackbacks');
 		}
+	}
+
+	public function removePostMetabox(string $postType): void
+	{
+		if (in_array($postType, self::EXCLUDE_POST_TYPES, true)) {
+			return;
+		}
+
+		remove_meta_box('commentstatusdiv', $postType, 'normal');
+		remove_meta_box('commentsdiv', $postType, 'normal');
 	}
 
 	public function removeAdminMenu(): void
@@ -75,54 +114,7 @@ class Comments implements Hookable
 		$wpAdminBar->remove_node('comments');
 	}
 
-	public function removePostMetabox(string $postType): void
-	{
-		remove_meta_box('commentstatusdiv', $postType, 'normal');
-		remove_meta_box('commentsdiv', $postType, 'normal');
-	}
-
-	/**
-	 * @param array<int>|array<WP_Comment>|null $comments
-	 *
-	 * @return array<int>|array<WP_Comment>|null
-	 */
-	public function commentsPreQuery(?array $comments): ?array
-	{
-		return null;
-	}
-
-	/**
-	 * @param array<string,string> $methods
-	 *
-	 * @return array<string,string>
-	 */
-	public function xmlrpcMethods(array $methods): array
-	{
-		unset($methods['wp.deleteComment']);
-		unset($methods['wp.editComment']);
-		unset($methods['wp.getComment']);
-		unset($methods['wp.getCommentCount']);
-		unset($methods['wp.getCommentStatusList']);
-		unset($methods['wp.getComments']);
-		unset($methods['wp.newComment']);
-
-		return $methods;
-	}
-
-	/**
-	 * @param array<string,mixed> $endpoints
-	 *
-	 * @return array<string,mixed>
-	 */
-	public function restEndpoints(array $endpoints): array
-	{
-		unset($endpoints['/wp/v2/comments']);
-		unset($endpoints['/wp/v2/comments/(?P<id>[\d]+)']);
-
-		return $endpoints;
-	}
-
-	public function removeBlocks(): void
+	public function unregisterBlocksServer(): void
 	{
 		$blocks = WP_Block_Type_Registry::get_instance()->get_all_registered();
 		$commentBlocks = [
@@ -152,7 +144,7 @@ class Comments implements Hookable
 		}
 	}
 
-	public function disableOnBlockEditor(): void
+	public function unregisterBlocksClient(): void
 	{
 		$assetFile = App::dir('dist/assets/comments/index.asset.php');
 
@@ -168,5 +160,132 @@ class Comments implements Hookable
 			$asset['version'],
 			true,
 		);
+	}
+
+	/**
+	 * @param array<string,string> $methods
+	 *
+	 * @return array<string,string>
+	 */
+	public function filterXmlrpcMethods(array $methods): array
+	{
+		unset($methods['wp.deleteComment']);
+		unset($methods['wp.editComment']);
+		unset($methods['wp.getComment']);
+		unset($methods['wp.getCommentCount']);
+		unset($methods['wp.getCommentStatusList']);
+		unset($methods['wp.getComments']);
+		unset($methods['wp.newComment']);
+
+		return $methods;
+	}
+
+	/**
+	 * @param array<string,mixed> $endpoints
+	 *
+	 * @return array<string,mixed>
+	 */
+	public function filterRestEndpoints(array $endpoints): array
+	{
+		unset($endpoints['/wp/v2/comments']);
+		unset($endpoints['/wp/v2/comments/(?P<id>[\d]+)']);
+
+		return $endpoints;
+	}
+
+	/**
+	 * @param array<int>|array<WP_Comment>|null $comments
+	 *
+	 * @return array<int>|array<WP_Comment>|null
+	 */
+	public function filterCommentsPreQuery(?array $comments, WP_Comment_Query $query): ?array
+	{
+		// phpcs:ignore Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps -- WordPress core variable.
+		if (self::isDashboardPage() && $query->query_vars['post_type'] === '') {
+			return [];
+		}
+
+		return $comments;
+	}
+
+	/**
+	 * Filter the number of comments to show on the "At a glance" widget in the
+	 * Dashboard.
+	 *
+	 * @param object|array<string,int> $count The comment count object or array.
+	 *
+	 * @return object|array<string,int>
+	 */
+	public function filterCommentsCount($count, int $postId)
+	{
+		if (
+			(self::isDashboardPage() && $postId === 0) ||
+			! in_array(get_post_type($postId), self::EXCLUDE_POST_TYPES, true)
+		) {
+			return (object) [
+				'approved' => 0,
+				'awaiting_moderation' => 0,
+				'spam' => 0,
+				'trash' => 0,
+				'post-trashed' => 0,
+				'total_comments' => 0,
+				'all' => 0,
+				'moderated' => 0,
+			];
+		}
+
+		return $count;
+	}
+
+	/**
+	 * Filter the output of comments to be rendered in the theme template.
+	 *
+	 * @param array<WP_Comment> $comments
+	 *
+	 * @return array<WP_Comment>
+	 */
+	public function filterCommentsArray(array $comments, int $postId): array
+	{
+		if (! in_array(get_post_type($postId), self::EXCLUDE_POST_TYPES, true)) {
+			return [];
+		}
+
+		return $comments;
+	}
+
+	/**
+	 * Filter whether the current post is open for comments.
+	 */
+	public function filterCommentsOpen(bool $isOpen, int $postId): bool
+	{
+		if (! in_array(get_post_type($postId), self::EXCLUDE_POST_TYPES, true)) {
+			return false;
+		}
+
+		return $isOpen;
+	}
+
+	/**
+	 * Filter whether the current post is open for pings.
+	 */
+	public function filterPingsOpen(bool $isOpen, int $postId): bool
+	{
+		if (! in_array(get_post_type($postId), self::EXCLUDE_POST_TYPES, true)) {
+			return false;
+		}
+
+		return $isOpen;
+	}
+
+	/**
+	 * Filter the number of comments a post has to show on the theme template.
+	 */
+	public function filterGetCommentsNumber(int $number, int $postId): int
+	{
+		if (! in_array(get_post_type($postId), self::EXCLUDE_POST_TYPES, true)) {
+			return 0;
+		}
+
+		return $number;
 	}
 }
