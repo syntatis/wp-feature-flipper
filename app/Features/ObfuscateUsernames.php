8e317b37978f1df3fcb852ac1a87fad5f94f6fcd
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-01-29 10:46:48 +0700

    Handle user query and username in REST API

--- app/Features/ObfuscateUsernames.php
+++ app/Features/ObfuscateUsernames.php
@@ -9,10 +9,12 @@ use SSFV\Codex\Foundation\Hooks\Hook;
 use SSFV\Symfony\Component\Uid\Uuid;
 use Syntatis\FeatureFlipper\Helpers\Option;
 use WP_Query;
+use WP_REST_Response;
 use WP_User;
 use WP_User_Query;
 
 use function count;
+use function is_array;
 use function is_string;
 use function sprintf;
 use function str_replace;
@@ -54,16 +56,25 @@ final class ObfuscateUsernames implements Hookable
 		);
 		$hook->addAction('pre_get_posts', [$this, 'preGetPosts'], PHP_INT_MAX);
 		$hook->addFilter('author_link', [$this, 'filterAuthorLink'], PHP_INT_MAX, 3);
+		$hook->addFilter('rest_prepare_user', [$this, 'filterRestPrepareUser'], PHP_INT_MAX, 2);
 	}
 
 	public function preGetPosts(WP_Query $query): void
 	{
-		// phpcs:disable Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps -- WordPress Core convention.
-		$authorName = $query->query_vars['author_name'];
-
 		if (! $query->is_author()) {
 			return;
 		}
+
+		/**
+		 * If the permalink structure is set to plain, the author should be queried
+		 * by the user ID.
+		 */
+		if ((bool) get_option('permalink_structure') === false) {
+			return;
+		}
+
+		// phpcs:disable Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps -- WordPress Core convention.
+		$authorName = $query->query_vars['author_name'];
 
 		if (! Uuid::isValid($authorName)) {
 			$query->is_404 = true;
@@ -90,8 +101,29 @@ final class ObfuscateUsernames implements Hookable
 		// phpcs:enable
 	}
 
+	public function filterRestPrepareUser(WP_REST_Response $response, WP_User $user): WP_REST_Response
+	{
+		$data = $response->get_data();
+
+		if (is_array($data)) {
+			$data['slug'] = get_user_meta($user->ID, self::USER_UUID_META_KEY, true);
+		}
+
+		$response->set_data($data);
+
+		return $response;
+	}
+
 	public function filterAuthorLink(string $link, int $userId, string $authorSlug): string
 	{
+		/**
+		 * Only change the author link when the permalink structure is not set to
+		 * plain.
+		 */
+		if ((bool) get_option('permalink_structure') === false) {
+			return $link;
+		}
+
 		$uuid = get_user_meta($userId, self::USER_UUID_META_KEY, true);
 
 		if (is_string($uuid) && Uuid::isValid($uuid)) {
