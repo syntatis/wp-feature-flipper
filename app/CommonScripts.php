22156c0d0e235855b5712ae99627d807e2281253
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-04-20 01:51:01 +0700

    Refactor some hooks with PHP Attribute

--- app/CommonScripts.php
+++ app/CommonScripts.php
@@ -6,16 +6,18 @@ namespace Syntatis\FeatureFlipper;
 
 use SSFV\Codex\Contracts\Hookable;
 use SSFV\Codex\Facades\App;
+use SSFV\Codex\Foundation\Hooks\Action;
 use SSFV\Codex\Foundation\Hooks\Hook;
 
 final class CommonScripts implements Hookable
 {
 	public function hook(Hook $hook): void
 	{
-		$hook->addAction('admin_enqueue_scripts', [$this, 'enqueueScripts']);
-		$hook->addAction('wp_enqueue_scripts', [$this, 'enqueueScripts']);
+		$hook->parse($this);
 	}
 
+	#[Action('admin_enqueue_scripts')]
+	#[Action('wp_enqueue_scripts')]
 	public function enqueueScripts(): void
 	{
 		if (! is_user_logged_in()) {
