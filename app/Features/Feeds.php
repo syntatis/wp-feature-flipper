e0deacfc9d6e510e3149f094ea5f3aade2a99d36
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-04-22 19:16:40 +0700

    Replace some hooks with PHP Attributes

--- app/Features/Feeds.php
+++ app/Features/Feeds.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Syntatis\FeatureFlipper\Features;
 
 use SSFV\Codex\Contracts\Hookable;
+use SSFV\Codex\Foundation\Hooks\Action;
 use SSFV\Codex\Foundation\Hooks\Hook;
 use Syntatis\FeatureFlipper\Helpers\Option;
 
@@ -18,24 +19,18 @@ final class Feeds implements Hookable
 			return;
 		}
 
-		// Disable feeds.
-		$hook->addAction('do_feed', [$this, 'toHomepage'], PHP_INT_MIN);
-		$hook->addAction('do_feed_rdf', [$this, 'toHomepage'], PHP_INT_MIN);
-		$hook->addAction('do_feed_rss', [$this, 'toHomepage'], PHP_INT_MIN);
-		$hook->addAction('do_feed_rss2', [$this, 'toHomepage'], PHP_INT_MIN);
-		$hook->addAction('do_feed_atom', [$this, 'toHomepage'], PHP_INT_MIN);
-
-		// Disable comments feeds.
-		$hook->addAction('do_feed_rss2_comments', [$this, 'toHomepage'], PHP_INT_MIN);
-		$hook->addAction('do_feed_atom_comments', [$this, 'toHomepage'], PHP_INT_MIN);
-
-		// Remove RSS feed links.
-		$hook->addFilter('feed_links_show_posts_feed', '__return_false', 99);
-
-		// Remove all extra RSS feed links.
-		$hook->addFilter('feed_links_show_comments_feed', '__return_false', 99);
+		$hook->parse($this);
+		$hook->addFilter('feed_links_show_posts_feed', '__return_false', 99); // Remove RSS feed links.
+		$hook->addFilter('feed_links_show_comments_feed', '__return_false', 99); // Remove all extra RSS feed links.
 	}
 
+	#[Action(name: 'do_feed', priority: PHP_INT_MIN)]
+	#[Action(name: 'do_feed_rdf', priority: PHP_INT_MIN)]
+	#[Action(name: 'do_feed_rss', priority: PHP_INT_MIN)]
+	#[Action(name: 'do_feed_rss2', priority: PHP_INT_MIN)]
+	#[Action(name: 'do_feed_atom', priority: PHP_INT_MIN)]
+	#[Action(name: 'do_feed_rss2_comments', priority: PHP_INT_MIN)]
+	#[Action(name: 'do_feed_atom_comments', priority: PHP_INT_MIN)]
 	public function toHomepage(): void
 	{
 		wp_redirect(home_url());
