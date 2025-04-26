9e7d5d5f6913b26a91fbc591bb41f6923146ebfc
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-04-26 18:23:26 +0700

    Refactor comment length validation tests and update exception messages

--- tests/phpunit/app/Modules/GeneralTest.php
+++ tests/phpunit/app/Modules/GeneralTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Syntatis\Tests\Modules;
 
+use Faker\Factory;
+use Faker\Generator;
 use SSFV\Codex\Foundation\Hooks\Hook;
 use Syntatis\FeatureFlipper\Helpers\Option;
 use Syntatis\FeatureFlipper\Modules\General;
@@ -17,6 +19,7 @@ class GeneralTest extends WPTestCase
 {
 	private Hook $hook;
 	private General $instance;
+	private static Generator $faker;
 
 	// phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps -- WordPress convention.
 	public function set_up(): void
@@ -26,6 +29,7 @@ class GeneralTest extends WPTestCase
 		$this->hook = new Hook();
 		$this->instance = new General();
 		$this->instance->hook($this->hook);
+		$this->faker = Factory::create();
 	}
 
 	/** @testdox should have callback attached to hooks */
@@ -87,7 +91,7 @@ class GeneralTest extends WPTestCase
 	public function testFilterPreprocessCommentFailed(): void
 	{
 		$this->expectException(WPDieException::class);
-		$this->expectExceptionMessage('Comment&#039;s too short. Please add something more helpful.');
+		$this->expectExceptionMessage('Comment&#039;s too short. Please write something more helpful.');
 
 		$this->instance->filterPreprocessComment(['comment_content' => 'hi']);
 	}
@@ -133,19 +137,92 @@ class GeneralTest extends WPTestCase
 
 	/**
 	 * @group feature-comments
-	 * @testdox should return the number of revisions to keep
+	 * @testdox should return the comment value or fail the comment is too short (default: 10 characters)
 	 */
-	public function testFilterPreprocessCommentUpdatedLengthFailed(): void
+	public function testMinCommentLengthDefault(): void
 	{
-		$comment = $this->instance->filterPreprocessComment(['comment_content' => 'hello world!']);
+		$comment = $this->instance->filterPreprocessComment(['comment_content' => 'hello world!']); // 12 characters.
 
 		$this->assertEquals(['comment_content' => 'hello world!'], $comment);
 
+		$this->expectException(WPDieException::class);
+		$this->expectExceptionMessage('Comment&#039;s too short. Please write something more helpful.');
+
+		$this->instance->filterPreprocessComment(['comment_content' => 'Hi!']); // 3 characters.
+	}
+
+	/**
+	 * @group feature-comments
+	 * @testdox should return the comment value or fail the comment is too short (updated: 20 characters)
+	 */
+	public function testMinCommentLengthUpdated(): void
+	{
 		Option::update('comment_min_length', 20);
 
-		$this->expectException(WPDieException::class);
-		$this->expectExceptionMessage('Comment&#039;s too short. Please add something more helpful.');
+		$this->assertSame(20, Option::get('comment_min_length'));
 
-		$this->instance->filterPreprocessComment(['comment_content' => 'hello world!']);
+		// Asserting with value about 20 characters.
+		$text = $this->faker->realTextBetween(20, 30);
+		$this->assertEquals(
+			['comment_content' => $text],
+			$this->instance->filterPreprocessComment(
+				['comment_content' => $text],
+			),
+		);
+
+		$this->expectException(WPDieException::class);
+		$this->expectExceptionMessage('Comment&#039;s too short. Please write something more helpful.');
+
+		$comment = $this->instance->filterPreprocessComment(['comment_content' => $this->faker->text(19)]);
+	}
+
+	/**
+	 * @group feature-comments
+	 * @testdox should return the comment value or fail when the comment is too long (default: 100 characters)
+	 */
+	public function testMaxCommentLengthDefault(): void
+	{
+		$this->assertSame(100, Option::get('comment_max_length'));
+
+		// Asserting with value about 100 characters.
+		$text = $this->faker->text(100);
+		$this->assertEquals(
+			['comment_content' => $text],
+			$this->instance->filterPreprocessComment(
+				['comment_content' => $text],
+			),
+		);
+
+		$this->expectException(WPDieException::class);
+		$this->expectExceptionMessage('Comment&#039;s too long. Please write more concisely.');
+
+		// Asserting with value about 101 or more characters.
+		$this->instance->filterPreprocessComment(['comment_content' => $this->faker->realTextBetween(101, 200)]);
+	}
+
+	/**
+	 * @group feature-comments
+	 * @testdox should return the comment value or fail when the comment is too long (300 characters)
+	 */
+	public function testMaxCommentLengthUpdated(): void
+	{
+		Option::update('comment_max_length', 300);
+
+		$this->assertSame(300, Option::get('comment_max_length'));
+
+		// Asserting with value about 300 characters.
+		$text = $this->faker->text(300);
+		$this->assertEquals(
+			['comment_content' => $text],
+			$this->instance->filterPreprocessComment(
+				['comment_content' => $text],
+			),
+		);
+
+		$this->expectException(WPDieException::class);
+		$this->expectExceptionMessage('Comment&#039;s too long. Please write more concisely.');
+
+		// Asserting with value about 301 or more characters.
+		$this->instance->filterPreprocessComment(['comment_content' => $this->faker->realTextBetween(300, 400)]);
 	}
 }
