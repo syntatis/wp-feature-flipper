58b5420bf8f7724a41d56499490bd85f30209c1b
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-08-12 07:32:44 +0700

    Init `PublicSearch`

---
+++ app/Features/PublicSearch.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Syntatis\FeatureFlipper\Features;
+
+use SSFV\Codex\Contracts\Hookable;
+use SSFV\Codex\Foundation\Hooks\Hook;
+use WP_Error;
+use WP_REST_Request;
+use WP_REST_Server;
+
+use function strpos;
+
+use const PHP_INT_MIN;
+
+final class PublicSearch implements Hookable
+{
+	public function hook(Hook $hook): void
+	{
+		$hook->addAction('widgets_init', static fn () => unregister_widget('WP_Widget_Search'));
+		$hook->addFilter('rest_pre_dispatch', [self::class, 'disableEndpoint'], PHP_INT_MIN, 3);
+		$hook->addFilter('get_search_form', '__return_empty_string');
+		$hook->addFilter('pre_get_posts', static function ($query): void {
+			if (self::isSearchQuery()) {
+				wp_safe_redirect(home_url(), 301);
+				exit;
+			}
+		});
+	}
+
+	/**
+	 * @param mixed                         $result
+	 * @param WP_REST_Request<array<mixed>> $request
+	 *
+	 * @return mixed
+	 */
+	public static function disableEndpoint($result, WP_REST_Server $server, WP_REST_Request $request)
+	{
+		if (strpos($request->get_route(), '/wp/v2/search') !== false) {
+			return new WP_Error(
+				'rest_no_route',
+				'The search endpoint is disabled.',
+				['status' => 404],
+			);
+		}
+
+		return $result;
+	}
+
+	private static function isSearchQuery(): bool
+	{
+		return is_search() && ! is_admin();
+	}
+}
