73ff8a20d5262431bbe68e259f0524d457d1e17b
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-08-16 13:25:56 +0700

    Init redirect

--- app/Features/PasswordReset.php
+++ app/Features/PasswordReset.php
@@ -6,11 +6,16 @@ namespace Syntatis\FeatureFlipper\Features;
 
 use SSFV\Codex\Contracts\Hookable;
 use SSFV\Codex\Foundation\Hooks\Hook;
+use Syntatis\FeatureFlipper\Helpers\URL;
+
+use function function_exists;
+use function trim;
 
 final class PasswordReset implements Hookable
 {
 	public function hook(Hook $hook): void
 	{
+		$hook->addAction('wp', [self::class, 'redirect']);
 		$hook->addFilter('allow_password_reset', '__return_false');
 		$hook->addFilter('gettext', [self::class, 'removeLostPasswordText']);
 	}
@@ -22,5 +27,29 @@ final class PasswordReset implements Hookable
 		}
 
 		return $text;
+	}
+
+	public static function redirect(): void
+	{
+		if (is_user_logged_in()) {
+			return;
+		}
+
+		if (did_action('woocommerce_loaded') === 1 && function_exists('is_wc_endpoint_url') && is_wc_endpoint_url('lost-password')) {
+			$accountPage = trim(wc_get_page_permalink('myaccount'));
+
+			if ($accountPage !== '') {
+				wp_safe_redirect($accountPage, 302);
+				exit;
+			}
+
+			wp_safe_redirect(home_url(), 302);
+			exit;
+		}
+
+		if (URL::isLogin() && ($_GET['action'] ?? '') === 'lostpassword') {
+			wp_safe_redirect(wp_login_url(), 302);
+			exit;
+		}
 	}
 }
